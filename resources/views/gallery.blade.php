0476be25f3d2899c9c1ef3f37a711419c3d5ad61
imran haider <imran.h@nixxieinternational.com>
2024-11-02 14:44:34 +0400

    updated gallery

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -3,13 +3,13 @@
 @section('title', 'Gondal Rice: Gallery')
 
 @section('assets')
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/css/lightbox.min.css">
 @endsection
 
 @section('content')
 
     <!-- Title page -->
-    <section class="how-overlay2 bg-img1" style="background-image: url('{{asset('assets/images/bg-07.jpg')}}');">
+    <section class="how-overlay2 bg-img1" style="background-image: url('{{ asset('assets/images/bg-07.jpg') }}');">
         <div class="container">
             <div class="txt-center p-t-160 p-b-165">
                 <h2 class="txt-l-101 cl0 txt-center p-b-14 respon1">
@@ -17,14 +17,14 @@
                 </h2>
 
                 <span class="txt-m-201 cl0 flex-c-m flex-w">
-					<a href="{{url('/')}}" class="txt-m-201 cl0 hov-cl10 trans-04 m-r-6">
-						Home
-					</a>
+                    <a href="{{ url('/') }}" class="txt-m-201 cl0 hov-cl10 trans-04 m-r-6">
+                        Home
+                    </a>
 
-					<span>
-						/ Gallery
-					</span>
-				</span>
+                    <span>
+                        / Gallery
+                    </span>
+                </span>
             </div>
         </div>
     </section>
@@ -32,51 +32,27 @@
     <!-- Gallery -->
     <div class="sec-gallery bg0 p-t-145 p-b-98">
         <div class="container">
-            {{--<div class="flex-w flex-c-m filter-tope-group p-b-47">
-                <button class="txt-m-104 cl9 hov2 trans-04 p-rl-22 p-b-10 how-active1" data-filter="*">
-                    All
-                </button>
-
-                <button class="txt-m-104 cl9 hov2 trans-04 p-rl-22 p-b-10" data-filter=".vegetable-fill">
-                    Super Kernel Basmati
-                </button>
-
-                <button class="txt-m-104 cl9 hov2 trans-04 p-rl-22 p-b-10" data-filter=".fruit-fill">
-                    Super kernel Sella
-                </button>
-
-                <button class="txt-m-104 cl9 hov2 trans-04 p-rl-22 p-b-10" data-filter=".fruit-juic-fill">
-                    Brown Rice
-                </button>
-
-                <button class="txt-m-104 cl9 hov2 trans-04 p-rl-22 p-b-10" data-filter=".dried-fill">
-                    Kainat Rice
-                </button>
-
-                <button class="txt-m-104 cl9 hov2 trans-04 p-rl-22 p-b-10" data-filter=".other-fill">
-                    Tota Rice
-                </button>
-            </div>--}}
-
             <div class="row gallery-lb isotope-grid isotope-grid-gallery">
                 @foreach($galleries as $gallery)
                     <div class="col-md-6 col-lg-4 p-b-30 isotope-item">
                         <div class="gallery-item wrap-pic-w">
-                            <img src="{{asset('assets/images/galleries/'.$gallery->image)}}" alt="GALLERY">
-
+                            <a href="{{ asset('assets/images/galleries/'.$gallery->image) }}" data-lightbox="gallery" data-title="{{ $gallery->name }}">
+                                <img src="{{ asset('assets/images/galleries/'.$gallery->image) }}" alt="GALLERY" class="img-fluid">
+                            </a>
+                            
                             <div class="gallery-overlay flex-c-m trans-04">
                                 <div class="gallery-content flex-w flex-c-m w-full">
-                                    <a class="flex-c-m gallery-btn m-all-5 trans-04 js-show-gallery js-gallery" href="images/gallery-01.jpg">
-                                        <img src="{{asset('assets/images/icons/icon-open.png')}}" alt="OPEN">
+                                    <a class="flex-c-m gallery-btn m-all-5 trans-04" href="{{ asset('assets/images/galleries/'.$gallery->image) }}" data-lightbox="gallery" data-title="{{ $gallery->name }}">
+                                        <img src="{{ asset('assets/images/icons/icon-open.png') }}" alt="OPEN">
                                     </a>
 
                                     <a href="#" class="flex-c-m gallery-btn m-all-5 trans-04">
-                                        <img src="{{asset('assets/images/icons/icon-link.png')}}" alt="LINK">
+                                        <img src="{{ asset('assets/images/icons/icon-link.png') }}" alt="LINK">
                                     </a>
 
                                     <div class="gallery-txt flex-col-c p-rl-15 p-t-10 trans-04">
                                         <span class="txt-m-109 cl0 txt-center">
-                                            {{$gallery->name}}
+                                            {{ $gallery->name }}
                                         </span>
 
                                         <span class="txt-s-106 cl0 txt-center">
@@ -88,25 +64,11 @@
                         </div>
                     </div>
                 @endforeach
-
             </div>
 
             <!-- Pagination -->
             <div class="flex-w flex-c-m p-t-48">
                 {{ $galleries->links() }}
-                {{--<a href="#" class="flex-c-m txt-s-115 cl6 size-a-23 bo-all-1 bocl15 hov-btn1 trans-04 m-all-3 active-pagi1">
-                    1
-                </a>
-
-                <a href="#" class="flex-c-m txt-s-115 cl6 size-a-23 bo-all-1 bocl15 hov-btn1 trans-04 m-all-3">
-                    2
-                </a>
-
-                <a href="#" class="flex-c-m txt-s-115 cl6 size-a-24 how-btn1 bo-all-1 bocl15 hov-btn1 trans-04 m-all-3 p-b-1">
-                    Next
-                    <span class="lnr lnr-chevron-right m-t-3 m-l-7"></span>
-                    <span class="lnr lnr-chevron-right m-t-3"></span>
-                </a>--}}
             </div>
         </div>
     </div>
@@ -114,5 +76,5 @@
 @endsection
 
 @section('scripts')
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/js/lightbox-plus-jquery.min.js"></script>
 @endsection
